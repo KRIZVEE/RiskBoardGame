23ba0a1fa089458f6fa10c3f32b95d44928d5fb8
charan121317 <charanpreetsingh127@gmail.com>
2017-10-12 23:36:43 -0400

    Changes of charanpreet on 12th October

--- RiskFX/src/in/risk/utility/Continent.java
+++ RiskFX/src/in/risk/utility/Continent.java
@@ -1,11 +1,17 @@
 package in.risk.utility;
 
+import java.util.Vector;
+
 public class Continent {
 
 	private String name;
+	private int value;
+	//private Vector<String> territories;
+	Territory t;
 	
-	public Continent(String name) {
+	 Continent(String name,int v) {
 		this.name = name;
+		value = v;
 	}
 
 	public String getName() {
@@ -15,6 +21,37 @@ public class Continent {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 	
+	/*public Vector<String> getTerritories(){
+		return territories;
+	}
+	
+	public boolean isTerritoryOf(Territory t){
+		return(territories.contains(t.getId()));
+	}*/
+	
+	/*public boolean isContinentcaptured(Player p){
+		Vector<Integer> t1 = new Vector<Integer>();
+		Vector<Territory> t2 = p.getOccupiedTerritories();
+		
+		for(int c=0 ;c<t2.size();c++){
+			t1.add(t2.elementAt(c).getId());
+			
+			for(int i=0; i<territories.size();i++){
+				if(!t1.contains(territories.elementAt(i))){
+					return false;
+				}
+			}
+		}
+		return true;
+	}*/
 	
 }
