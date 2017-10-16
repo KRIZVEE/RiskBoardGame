5824e8cb55a698b799b39c4025c733f19f771475
charan121317 <charanpreetsingh127@gmail.com>
2017-10-16 03:51:37 -0400

    New interface for map editor

---
+++ RiskGame/src/in/risk/gui/MapEditorInterface.java
@@ -0,0 +1,252 @@
+package in.risk.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.risk.utility.RiskGame;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MapEditorInterface {
+	
+	static RiskGame objRiskGame = new RiskGame();
+	
+	public static List<String> editedContinents = new ArrayList<String>();
+	public static List<String> editedCountries = new ArrayList<String>();
+	
+	Button editContinents;
+	Button editCountries;
+	Button back;
+	Button saveMap;
+	
+	public  void mapEditorInterface(){
+		
+		Stage mapEditorStage = new Stage();
+		
+		Image logo = new Image(RiskInterface.logoPath);
+		ImageView imgview  = new ImageView(logo);
+		
+		editContinents = new Button("Edit Continents");
+		
+		editContinents.setOnAction(new EventHandler<ActionEvent>(){
+			
+			@Override
+			public void handle(ActionEvent event){
+				editContinentsInterface();
+			}
+		});
+		editCountries = new Button("Edit Countries");
+		
+		editCountries.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				editCountriesInterface();
+				
+			}
+		});
+		
+		back = new Button("Back");
+		
+		back.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				mapEditorStage.close();
+				
+			}
+		});
+		
+		VBox vbox = new VBox();
+		vbox.getStyleClass().add("vbox");
+		HBox hbox = new HBox();
+		hbox.getStyleClass().add("hbox");
+		hbox.getChildren().addAll(editContinents,editCountries);
+		vbox.getChildren().addAll(imgview,hbox,back);
+		
+		
+		Group mapEdiorRoot = new Group();
+		mapEdiorRoot.getChildren().addAll(vbox);
+		Scene mapEditorScene = new Scene(mapEdiorRoot,500,400);
+		mapEditorScene.getStylesheets().add(RiskInterface.css);
+		mapEditorStage.setTitle("Risk Game: Map Editor");
+		mapEditorStage.setScene(mapEditorScene);
+		mapEditorStage.centerOnScreen();
+		mapEditorStage.show();
+		
+		
+	}
+	
+	public static void editContinentsInterface(){
+				
+		Stage continentEditorStage = new Stage();
+		
+		Image logo = new Image(RiskInterface.logoPath);
+		ImageView imageView = new ImageView(logo);	
+		
+		Label lblAdd = new Label("Add a new continent: ");
+		TextField fieldAdd = new TextField();
+		fieldAdd.setPromptText("Enter Continent Name");
+		Label lblDel = new Label("Delete a continent: ");
+		Button add = new Button("Add");
+		Button del = new Button("Delete");
+		Button submit = new Button("Submit");
+		
+		ObservableList<String> continentsList = FXCollections.observableArrayList(objRiskGame.continentList);
+		ComboBox<String> conListBox = new ComboBox<>(continentsList);
+		conListBox.setPromptText("select Continent");
+		
+		HBox hbox1 = new HBox();
+		hbox1.getStyleClass().add("hbox");
+		hbox1.getChildren().addAll(lblAdd,fieldAdd,add);
+		
+		HBox hbox2 = new HBox();
+		hbox2.getStyleClass().add("hbox");
+		hbox2.getChildren().addAll(lblDel,conListBox,del);
+		VBox vbox = new VBox();
+		vbox.getStyleClass().add("vbox");
+		vbox.getChildren().addAll(imageView,hbox1,hbox2,submit);
+		Group continentEditorRoot  = new Group();
+		continentEditorRoot.getChildren().addAll(vbox);
+		
+		add.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event){
+				if(!fieldAdd.getText().isEmpty()){
+					
+					continentsList.add(fieldAdd.getText());
+				}					
+					fieldAdd.clear();
+			}
+		});
+		
+		del.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				continentsList.remove(conListBox.getValue());
+				
+			}
+		});
+		
+		submit.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				
+				for(int i = 0;i <continentsList.size();i++){
+					editedContinents.add(continentsList.get(i));
+				}
+				continentEditorStage.close();
+				
+			}
+		});
+		
+		Scene conEditorScene = new Scene(continentEditorRoot,500,400);
+		conEditorScene.getStylesheets().add(RiskInterface.css);
+		continentEditorStage.setTitle("Risk Game: Edit Continents");
+		continentEditorStage.setScene(conEditorScene);
+		continentEditorStage.centerOnScreen();
+		continentEditorStage.show();		
+		
+	}
+	
+	public static void editCountriesInterface(){
+		
+		Stage countryEditorStage = new Stage();
+		
+		Image logo = new Image(RiskInterface.logoPath);
+		ImageView imgview = new ImageView(logo);
+		ObservableList<String> continentList;
+		
+		if(!editedContinents.isEmpty()){
+			continentList = FXCollections.observableArrayList(editedContinents);
+		}else
+			continentList = FXCollections.observableArrayList(objRiskGame.continentList);
+		
+		ComboBox<String> continentBox =new ComboBox<String>(continentList);
+		continentBox.setPromptText("Select Continent");
+		
+		ObservableList<String> countryList = FXCollections.observableArrayList(objRiskGame.countryList);
+		ComboBox<String> countryBox = new ComboBox<String>(countryList);
+		countryBox.setPromptText("Select Country");
+		
+		Label lblAdd = new Label("Add Country: ");
+		TextField fieldAdd = new TextField(); 
+		fieldAdd.setPromptText("Enter Country Name");
+		Label lblDel  = new Label("Delete Country: ");
+		
+		Button add = new Button("Add");
+		Button del = new Button("Delete");
+		Button submit = new Button("Submit");
+		
+		HBox hbox1 = new HBox();
+		hbox1.getStyleClass().add("hbox");
+		hbox1.getChildren().addAll(lblAdd,fieldAdd,continentBox,add);
+		
+		HBox hbox2 = new HBox();
+		hbox2.getStyleClass().add("hbox");
+		hbox2.getChildren().addAll(lblDel,countryBox,del);
+		
+		VBox vbox = new VBox();
+		vbox.getStyleClass().add("vbox");
+		vbox.getChildren().addAll(imgview,hbox1,hbox2,submit);
+		
+		add.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				if(!fieldAdd.getText().isEmpty() && !continentBox.getValue().isEmpty()){
+					countryList.add(fieldAdd.getText());
+				}
+				fieldAdd.clear();
+			}
+		});
+		
+		del.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				countryList.remove(countryBox.getValue());
+				
+			}
+		});
+		
+		submit.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				for(int i=0;i<countryList.size();i++){
+					editedCountries.add(countryList.get(i));
+				}
+				countryEditorStage.close();
+				
+			}
+		});
+		
+		Group countryEditorRoot = new Group();
+		countryEditorRoot.getChildren().addAll(vbox);
+		Scene countryEditorscene = new Scene(countryEditorRoot,500,400);
+		countryEditorscene.getStylesheets().add(RiskInterface.css);
+		countryEditorStage.setTitle("Risk Game: Edit Countries");
+		countryEditorStage.setScene(countryEditorscene);
+		countryEditorStage.centerOnScreen();
+		countryEditorStage.show();
+		
+	}
+
+}
--- RiskGame/src/in/risk/gui/RiskInterface.java
+++ RiskGame/src/in/risk/gui/RiskInterface.java
@@ -2,174 +2,69 @@ package in.risk.gui;
 	
 import in.risk.utility.RiskGame;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class RiskInterface extends Application {
 
+public class RiskInterface extends Application {
+    
+    public static String css = "file:///E:/workSpace/RiskGame/src/in/risk/gui/application.css";
+    public static String logoPath = "file:///E:/workSpace/RiskGame/resources/Risk_logo.png";
+	
+	MapEditorInterface objMapEditorInterface = new MapEditorInterface();
+
+	Button mapEditor ;
+	Button addPlayer ;
+	Button startGame ;
+	
+	Image logo  = new Image(logoPath);
+	ImageView imgView = new ImageView();
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		RiskGame objRiskGame = new RiskGame();
+		mapEditor = new Button("Map Editor");
+		addPlayer = new Button("Add Players");
+		startGame  = new Button("Start Game");
 		
-				// Setting up text
-				Text text1 = new Text("Continents: ");
-				// Setting labels
-				Label label1 = new Label();
-				Label label2 = new Label();
-				Label label3 = new Label();
-				Label label4 = new Label();
-				Label label5 = new Label();
-				Label label6 = new Label();
-				Label label7 = new Label();
-				////////////////////////////////////////////////////////
-				////////////////////////////////////////////////////////
-				////////////// see here //////////////
-				////////////////////////////////////////////////////////
-				////////////////////////////////////////////////////////
-				// Setting textbox
-				TextField continentname;
-				continentname = new TextField();
-				continentname.setPromptText("Please enter continent name");
-				// continentname.setText("Continent");
-				continentname.setVisible(false);
-			
-
-				if (continentname.getText().trim().equals("")) {
-					System.out.println("textField is empty");
-				}
-				System.out.println("print before");
-				System.out.println(continentname.getText());
-				System.out.println("print after");
-				
-				TextField countryname = new TextField();
-				countryname.setVisible(false);
-			
-				ObservableList<String> numOfcontinent = FXCollections.observableArrayList(objRiskGame.con);
-				ComboBox box1 = new ComboBox(numOfcontinent);
-				box1.setVisible(false);
-
-				// drop down list for continents for country
-				ObservableList<String> countrylist = FXCollections.observableArrayList(objRiskGame.terr);
-				ComboBox box3 = new ComboBox(countrylist);
-				box3.setVisible(false);
-				// Add button
-				Button add = new Button("Add");
-				Button delete = new Button("Delete");
-				Button nextBtn = new Button("Next");
-				Button addcoun = new Button("Add");
-				addcoun.setVisible(false);
-				Button delcoun = new Button("Delete");
-				delcoun.setVisible(false);
-				Button save = new Button("Save");
-				save.setVisible(false);
-				// Set actions on Add button
-				add.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						// TODO Auto-generated method stub
-						label1.setText("Name: ");
-						continentname.localToScene(170.0, 170.0);
-						continentname.setVisible(true);
-					}
-				});
-				// Set action on Delete button
-				delete.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						// TODO Auto-generated method stub
-						label2.setText("Continent List: ");
-						box1.setVisible(true);
-					}
-				});
-				// Set action on next button
-				nextBtn.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						// TODO Auto-generated method stub
-						label3.setText("Country: ");
-						label7.setText("Adjacency: ");
-						addcoun.setVisible(true);
-						delcoun.setVisible(true);
-						save.setVisible(true);
-						// ====> need to work on this====>
-						// myObject.add(continentname.getText());
+		imgView.setImage(logo);
+		imgView.setFitWidth(300);
+		imgView.setPreserveRatio(true);
+		imgView.setSmooth(true);
+		imgView.setCache(true);
+		
+		VBox vbox = new VBox();
+		vbox.getStyleClass().add("vbox");
+		vbox.getChildren().addAll(imgView,mapEditor,addPlayer,startGame);
+		
+		
+		Group root = new Group();
+		root.getChildren().add(vbox);
+		Scene scene = new Scene(root,600,500);
+		
+		scene.getStylesheets().add(css);
 	
-						
-					}
-				});
-				ObservableList<String> numOfcontinent1 = FXCollections.observableArrayList(objRiskGame.con);
-				ComboBox box2 = new ComboBox(numOfcontinent1);
-				box2.setVisible(false);
-				// set action on add country button
-				addcoun.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						// TODO Auto-generated method stub
-						label4.setText("Name: ");
-						numOfcontinent1.add(continentname.getText());
-						box2.setVisible(true);
-						countryname.setVisible(true);
-						label5.setText("Continent List: ");
-						System.out.println("c add btn");
-					}
-				});
-				// Set action on delete country button
-				delcoun.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						// TODO Auto-generated method stub
-						label6.setText("Country List: ");
-						box3.setVisible(true);
-					}
-				});
-				GridPane grid1 = new GridPane();
-				grid1.setMinSize(700, 800);
-				grid1.setVgap(6);
-				grid1.setHgap(6);
-				grid1.setAlignment(Pos.CENTER);
-				grid1.add(text1, 0, 0);
-				grid1.add(add, 0, 1);
-				grid1.add(label1, 1, 1);
-				grid1.add(continentname, 2, 1);
-				grid1.add(delete, 0, 4);
-				grid1.add(label2, 1, 4);
-				grid1.add(box1, 2, 4);
-				grid1.add(nextBtn, 0, 5);
-				grid1.add(label3, 0, 7);
-				grid1.add(addcoun, 0, 8);
-				grid1.add(delcoun, 0, 9);
-				grid1.add(label4, 1, 8);
-				grid1.add(countryname, 2, 8);
-				grid1.add(label5, 3, 8);
-				grid1.add(box2, 4, 8);
-				grid1.add(label6, 1, 9);
-				grid1.add(box3, 2, 9);
-				grid1.add(label7, 0, 12);
-				grid1.add(save, 0, 14);
-
-				// Creating scene
-				Group root = new Group();
-				ObservableList list = root.getChildren();
-				list.add(grid1);
-				Scene scene1 = new Scene(root, 600, 600);
-				primaryStage.setTitle("Risk Game");
-				primaryStage.setScene(scene1);
-				primaryStage.show();
+		primaryStage.setTitle("Risk Game");
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.centerOnScreen();
+		primaryStage.show();
 		
+		
+		mapEditor.setOnAction(new EventHandler<ActionEvent>() {			
+			@Override
+			public void handle(ActionEvent event) {
+				
+				objMapEditorInterface.mapEditorInterface();
+			}
+		});
 	}
 	
 	
