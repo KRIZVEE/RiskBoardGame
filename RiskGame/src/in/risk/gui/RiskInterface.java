7d865cd77b5908787b5952558853a49d4607e519
ishanstealth18 <ishanstealth18@yahoo.co.in>
2017-10-19 17:21:15 -0400

    Revert "Final build 1"

--- RiskGame/src/in/risk/gui/RiskInterface.java
+++ RiskGame/src/in/risk/gui/RiskInterface.java
@@ -1,133 +1,97 @@
 package in.risk.gui;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import in.risk.utility.MapLoader;
+	
 import in.risk.utility.RiskGame;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
- * This class is main interface for starting map editor.</br>
- * and starting the game phases.
- * 
- * @author Charanpreet Singh, Ishan Kansara, Kashif Rizvee, Mohit Rana
- * @version 1.0.0
+ * This class is related to first screen of main editor
+ * @author Kashif Rizvee
+ *
  */
 public class RiskInterface extends Application {
-
+    
+    
+	
 	MapEditorInterface objMapEditorInterface = new MapEditorInterface();
-	RiskGame objRiskGame = new RiskGame();
-	MapLoader objMapLoader = new MapLoader();
+	GamePhaseInterface objGamePhaseInterface = new GamePhaseInterface();
 
-	public static String pathMap;
-
-	Button mapEditor;
-	Button addPlayer;
-	Button startGame;
-
-	// Image logo = new Image(RiskGame.logoPath);
-	// ImageView imgView = new ImageView();
-
-	public ArrayList<String> path;
-
-	/**
-	 * This method invokes the initial stage.
-	 * 
-	 * @throws Exception
-	 */
+	Button mapEditor ;
+	Button addPlayer ;
+	Button startGame ;
+	
+	Image logo  = new Image(RiskGame.logoPath);
+	ImageView imgView = new ImageView();
+	
 	@Override
+	/**
+	 * This method executes layout for first screen or main screen of map editor.
+	 */
 	public void start(Stage primaryStage) throws Exception {
-
-		File dir = new File("resources/maps");
-
-		path = new ArrayList<String>(Arrays.asList(dir.list()));
-
+		
 		mapEditor = new Button("Map Editor");
 		addPlayer = new Button("Add Players");
-		startGame = new Button("Start Game");
-
-		// imgView.setImage(logo);
-		// imgView.setFitWidth(300);
-		// imgView.setPreserveRatio(true);
-		// imgView.setSmooth(true);
-		// imgView.setCache(true);
-
-		Text selectmap = new Text("Select Maps: ");
-
-		// Setting up combo box for selecting maps
-		ObservableList<String> maplist = FXCollections.observableArrayList(path);
-		ComboBox<String> maplist2 = new ComboBox<String>(maplist);
-		maplist2.setVisible(true);
-		maplist2.setMinWidth(200);
-
+		startGame  = new Button("Start Game");
+		
+		imgView.setImage(logo);
+		imgView.setFitWidth(300);
+		imgView.setPreserveRatio(true);
+		imgView.setSmooth(true);
+		imgView.setCache(true);
+		
 		VBox vbox = new VBox();
 		vbox.getStyleClass().add("vbox");
-		vbox.getChildren().addAll(selectmap, maplist2, mapEditor, addPlayer, startGame);
-
+		vbox.getChildren().addAll(imgView,mapEditor,addPlayer,startGame);
+		
+		
 		Group root = new Group();
-
 		root.getChildren().add(vbox);
-		Scene scene = new Scene(root, 600, 500);
-
-		// scene.getStylesheets().add(RiskGame.css);
-
+		Scene scene = new Scene(root,600,500);
+		
+		scene.getStylesheets().add(RiskGame.css);
+	
 		primaryStage.setTitle("Risk Game");
 		primaryStage.setScene(scene);
 		primaryStage.sizeToScene();
 		primaryStage.centerOnScreen();
 		primaryStage.show();
-
+		
 		startGame.setOnAction(new EventHandler<ActionEvent>() {
-
+			
 			@Override
 			public void handle(ActionEvent event) {
-				pathMap = maplist2.getValue();
-				System.out.println(pathMap);
-				try {
-					objRiskGame.startGame(pathMap);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				objGamePhaseInterface.gamePhaseInteface();
 				primaryStage.close();
-
-				MapLoader.mapLoader(pathMap);
 				MapEditorInterface.continentEditorStage.close();
 				MapEditorInterface.countryEditorStage.close();
 				MapEditorInterface.mapEditorStage.close();
 			}
 		});
-
-		mapEditor.setOnAction(new EventHandler<ActionEvent>() {
+		
+		mapEditor.setOnAction(new EventHandler<ActionEvent>() {			
 			@Override
 			public void handle(ActionEvent event) {
-				pathMap = maplist2.getValue();
-				MapLoader.mapLoader(pathMap);
+				
 				objMapEditorInterface.mapEditorInterface();
 			}
 		});
-	}
-
+	}	
 	/**
-	 * This method launches the main method
+	 * This method is main method where execution starts for layout of the screen.
+	 * @param args This parameter is used to pass arguments while launching of screen
 	 */
-
 	public static void main(String[] args) {
+		RiskGame objRiskGame =  new RiskGame();
 		launch(args);
-
+		
 	}
 }
+
--- RiskGame/src/in/risk/utility/MapWriter.java
+++ RiskGame/src/in/risk/utility/MapWriter.java
@@ -5,73 +5,50 @@ import java.io.PrintStream;
 
 import in.risk.gui.MapEditorInterface;
 
-/**
- * This class contains all the methods related to editing the map and writing
- * the mapfile.
- * 
- * @author Charanpreet Singh, Ishan Kansara, Kashif Rizvee, Mohit Rana
- * @version 1.0.0
- */
 public class MapWriter {
-	/**
-	 * This method is called in mapEditorInterface for writing the new map file.
-	 */
-	public static void mapWriter(String path) {
-		try {
-			String pathName = "resources/maps/" + path;
-			PrintStream outFile = new PrintStream(new FileOutputStream(pathName));
+	
+	public static void mapWriter(){
+		try{
+			PrintStream outFile = new PrintStream(new FileOutputStream("resources/new.map"));
 			outFile.println("[Map]");
-			for (int i = 0; i < MapLoader.mapDetail.size(); i++) {
+			for(int i = 0 ; i < MapLoader.mapDetail.size();i++){
 				outFile.println(MapLoader.mapDetail.get(i));
-			}
+			}	
 			outFile.println("");
 			outFile.println("[Continents]");
-			if (MapEditorInterface.editedContinents.isEmpty()) {
-				for (int i = 0; i < MapLoader.continentFilterNew.size(); i++) {
-					outFile.println(MapLoader.continentFilterNew.get(i).replace(" ", "_") + "="
-							+ (long) Math.floor(Math.random() * 10));
-				}
+			if(MapEditorInterface.editedContinents.isEmpty()){
+				for(int i=0;i<MapLoader.continentFilterNew.size();i++){
+					outFile.println(MapLoader.continentFilterNew.get(i).replace(" ", "_")+"="+(long)Math.floor(Math.random() * 10)  );
 			}
-			for (int i = 0; i < MapEditorInterface.editedContinents.size(); i++) {
-				outFile.println(MapEditorInterface.editedContinents.get(i).replace(" ", "_") + "="
-						+ (long) Math.floor(Math.random() * 10));
-
 			}
-
+				for(int i=0;i<MapEditorInterface.editedContinents.size();i++){
+					outFile.println(MapEditorInterface.editedContinents.get(i).replace(" ", "_")+"="+(long)Math.floor(Math.random() * 10)  );
+				
+			}
+			
 			outFile.println("");
 			outFile.println("[Territories]");
-			if (MapEditorInterface.editedCountries.isEmpty()) {
-				for (int i = 0; i < MapLoader.countryFilter.size(); i++) {
+			if(MapEditorInterface.editedCountries.isEmpty()){
+				for(int i =0; i < MapLoader.countryFilter.size();i++){
 					String s = MapLoader.adj.get(MapLoader.countryFilter.get(i)).toString();
-					String t = s.substring(1, s.length() - 1);
+					String t= s.substring(1, s.length()-1);
 					String z = t.replace(", ", ",");
-
-					outFile.println(MapLoader.countryFilter.get(i) + ","
-							+ MapLoader.countryCoordinates.get(MapLoader.countryFilter.get(i)).get(0) + ","
-							+ MapLoader.countryCoordinates.get(MapLoader.countryFilter.get(i)).get(1) + ","
-							+ MapLoader.countryContinent.get(MapLoader.countryFilter.get(i)) + "," + z);
+					
+					outFile.println(MapLoader.countryFilter.get(i)+","+MapLoader.countryCoordinates.get(MapLoader.countryFilter.get(i)).get(0)+","+MapLoader.countryCoordinates.get(MapLoader.countryFilter.get(i)).get(1)+","+MapLoader.countryContinent.get(MapLoader.countryFilter.get(i))+","+z);
 				}
-			} else {
-				for (int i = 0; i < MapEditorInterface.editedCountries.size(); i++) {
+			}else{
+				for(int i =0; i < MapEditorInterface.editedCountries.size();i++){
 					String s = MapEditorInterface.editedAdj.get(MapEditorInterface.editedCountries.get(i)).toString();
-					String t = s.substring(1, s.length() - 1);
+					String t= s.substring(1, s.length()-1);
 					String z = t.replace(", ", ",");
-
-					outFile.println(MapEditorInterface.editedCountries.get(i) + ","
-							+ MapEditorInterface.editedCountryCoordinates.get(MapEditorInterface.editedCountries.get(i))
-									.get(0)
-							+ ","
-							+ MapEditorInterface.editedCountryCoordinates.get(MapEditorInterface.editedCountries.get(i))
-									.get(1)
-							+ ","
-							+ MapEditorInterface.editedCountryContinents.get(MapEditorInterface.editedCountries.get(i))
-							+ "," + z);
+					
+					outFile.println(MapEditorInterface.editedCountries.get(i)+","+MapEditorInterface.editedCountryCoordinates.get(MapEditorInterface.editedCountries.get(i)).get(0)+","+MapEditorInterface.editedCountryCoordinates.get(MapEditorInterface.editedCountries.get(i)).get(1)+","+MapEditorInterface.editedCountryContinents.get(MapEditorInterface.editedCountries.get(i))+","+z);
 				}
-
+			
 			}
 			outFile.println(" ");
 			System.setOut(outFile);
-		} catch (Exception e) {
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
